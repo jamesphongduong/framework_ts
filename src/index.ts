670ef20786182d59c7a6665cc7301d7b8a376cef
James Duong <james@simble.io>
2019-11-13 11:01:46 +1100

    151

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { User } from "./models/User";
+import axios from "axios";
 
-const user = new User({ name: "myname", age: 20 });
+axios.post("http://localhost:3000/users", {
+  name: "myname",
+  age: 20
+});
 
-user.on("change", () => {});
-
-console.log(user);
+axios.get("http://localhost:3000/users/1");
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from "axios";
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
@@ -18,10 +21,30 @@ export class User {
     Object.assign(this.data, update);
   }
 
+  // registering events
   on(eventName: string, callback: Callback): void {
     const handlers = this.events[eventName] || [];
-    console.log("handlers", handlers);
     handlers.push(callback);
     this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => {
+      callback();
+    });
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((res: AxiosResponse): void => {
+        this.set(response.data);
+      });
   }
 }
