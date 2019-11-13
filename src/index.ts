776b32bcb1cc019916d8ca2c109e69be60890196
James Duong <james@simble.io>
2019-11-14 10:02:46 +1100

    153

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
+const user = new User({ name: "new record", age: 0 });
 
-user.fetch();
+// user.set({ name: "NEW ", age: 999 });
+
+user.save();
