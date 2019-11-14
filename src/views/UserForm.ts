b595bab7125afd0d74d0a8a5c640f721362aae7e
James Duong <james@simble.io>
2019-11-15 09:32:48 +1100

    195

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,31 +1,35 @@
-import { User } from "../models/User";
+import { User } from '../models/User';
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel():
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:button": this.onButtonClick,
-      "mouseenter:h1": this.onHeaderHover
+      'click:.set-age': this.onSetAgeClick
     };
   }
 
-  onButtonClick(): void {
-    console.log("hi");
-  }
-
-  onHeaderHover(): void {
-    console.log("H1 was hovered");
-  }
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
-        <div>User name: ${this.model.get("name")}</div>
-        <div>User name: ${this.model.get("age")}</div>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User name: ${this.model.get('age')}</div>
         <input />
         <button>click me</button>
+        <button class='set-age'>Set Random Age</button>
       </div>
     `;
   }
@@ -34,7 +38,7 @@ export class UserForm {
     const eventsMap = this.eventsMap();
 
     for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(":");
+      const [eventName, selector] = eventKey.split(':');
 
       fragment.querySelectorAll(selector).forEach(element => {
         element.addEventListener(eventName, eventsMap[eventKey]);
@@ -43,7 +47,9 @@ export class UserForm {
   }
 
   render(): void {
-    const templateElement = document.createElement("template");
+    this.parent.innerHTML = '';
+    
+    const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
